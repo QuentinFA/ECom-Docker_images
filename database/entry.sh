f005417cc2f11e8e87a81387762fe2d3e841bc88
QuentinFA <aoghar@gmail.com>
2016-11-18 09:09:44 +0100

    Add possibility to initialise database with a sql file.

--- database/entry.sh
+++ database/entry.sh
@@ -1,2 +1,6 @@
 service mysql restart
+
+# If a file db.sql exists, use it to initialize the database
+[ -f /mnt/db.sql ] && echo "Initialisation file detected" && mysql -uroot -proot < /mnt/db.sql
+
 mysql -uroot -proot
