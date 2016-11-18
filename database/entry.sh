6c81cb021e9c7ed53f913a694f8d19e9990232b8
QuentinFA <aoghar@gmail.com>
2016-11-18 14:08:26 +0100

    Update entry.sh : more explicit sql file name

--- database/entry.sh
+++ database/entry.sh
@@ -1,6 +1,6 @@
 service mysql restart
 
 # If a file db.sql exists, use it to initialize the database
-[ -f /mnt/db.sql ] && echo "Initialisation file detected" && mysql -uroot -proot < /mnt/db.sql
+[ -f /mnt/init.sql ] && echo "Initialisation file detected" && mysql -uroot -proot < /mnt/init.sql
 
 mysql -uroot -proot
