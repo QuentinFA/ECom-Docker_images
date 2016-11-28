dce4b2424e15157868cf46f37f2d3cedad82e840
QuentinFA <aoghar@gmail.com>
2016-11-28 14:56:36 +0100

    Changed Galera for MySQL cluster.

--- database/entry.sh
+++
@@ -1,6 +0,0 @@
-service mysql restart
-
-# If a file db.sql exists, use it to initialize the database
-[ -f /mnt/init.sql ] && echo "Initialisation file detected" && mysql -uroot -proot < /mnt/init.sql
-
-mysql -uroot -proot
